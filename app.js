5a207638079256d76585f7afa06a53d05005fe8e
thangzathang <thangzathang@gmail.com>
2021-11-09 10:19:08 +1100

    git init onlt to source

---
+++ app.js
@@ -0,0 +1,14 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', () => {
+        // Navbar Mobile Animation toggle
+        nav.classList.toggle('nav-active');
+
+        // Burger Animation
+        // burger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
