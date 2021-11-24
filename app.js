61c288fc44d86a91b1185af601bc382d485ac34e
thangzathang <thangzathang@gmail.com>
2021-11-24 12:02:42 +1100

    The new nav bar is implemented across the site ready for deployment

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
+// Desktop Navslide
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
 
-    burger.addEventListener('click', () => {
-        // Navbar Mobile Animation toggle
-        nav.classList.toggle('nav-active');
+  burger.addEventListener("click", () => {
+    // Navbar Mobile Animation toggle
+    nav.classList.toggle("nav-active");
 
-        // Burger Animation
-        // burger.classList.toggle('toggle');
-    });
-}
+    // Burger Animation
+    // burger.classList.toggle('toggle');
+  });
+};
 
 navSlide();
+
+// Mobile Navslide
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav__link");
+
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
+  });
+});
